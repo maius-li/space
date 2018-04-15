f9a246936b825ba34c4a1af8e8788f44ecfe731d
Maius <bow810531@gmail.com>
2018-04-15 15:56:22 +0800

    新增月球

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+// @import 'styles/App.scss';
+// @import 'App.scss';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <div className="moon-wrap"></div>
+      <div className="moon"></div>
         <div className="space-content">
-          <div className="moon"></div>
+          <div className="hole"></div>
+          <div className="hole"></div>
+          <div className="hole"></div>
         </div>
       </div>
     );
