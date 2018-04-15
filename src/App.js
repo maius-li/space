d35cfc369be83ddeb8d68c52f4cb8159814be056
Maius <bow810531@gmail.com>
2018-04-15 13:55:45 +0800

    星球環境

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="space-content">
+          <div className="moon"></div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
